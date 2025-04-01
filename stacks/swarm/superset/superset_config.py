15a79e734811ec5599cb452b2c5ce1b663a67e90
Bruno Peixoto <brunolnetto@gmail.com>
2025-04-01 09:02:41 -0300

    feat: containers testing and swarm stacks

---
+++ stacks/swarm/superset/superset_config.py
@@ -0,0 +1,6 @@
+# superset_config.py
+
+# Example configuration for Superset
+SQLALCHEMY_DATABASE_URI = 'postgresql://superset:superset@superset-db:5432/superset'
+SECRET_KEY = 'your_secret_key'
+SUPERSET_WEBSERVER_PORT = 8088
